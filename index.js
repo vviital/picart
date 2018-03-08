b1ef3ed5ecb54ace95095e8d6a209e075df14a5b
vviital <vviital@tut.by>
2018-03-08 12:19:25 +0300

    Add koa to the project

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-import { sequelize } from './src/database/index';
+// import { sequelize } from './src/database/index';
 
-(async () => {
-  await sequelize.authenticate();
-})().then(console.log).catch(console.error);
+// (async () => {
+//   await sequelize.authenticate();
+// })().then(console.log).catch(console.error);
+
+import Koa from 'koa';
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
